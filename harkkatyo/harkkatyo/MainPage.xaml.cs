ecfe054ee038b97da2cf6b0625714aedf3308e80
Ville Halminen <vhalminen@gmail.com>
2017-03-17 17:58:23 +0200

    Versio 0.1 harkkatyosta

---
+++ harkkatyo/harkkatyo/MainPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace harkkatyo
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        Opettaja Ari = new Opettaja("Ari", 2000);
+        Opettaja Narsu = new Opettaja("Narsu", 5);
+        Opettaja Jarmo = new Opettaja("Jarmo", 3000);
+        Opettaja Mieskolainen = new Opettaja("Matti", 100000);
+        
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            nimi1TextBlock.Text = Ari.Nimi;
+            palkka1TextBlock.Text = Ari.Palkka.ToString();
+            nimi2TextBlock.Text = Narsu.Nimi;
+            palkka2TextBlock.Text = Narsu.Palkka.ToString();
+            nimi3TextBlock.Text = Jarmo.Nimi;
+            palkka3TextBlock.Text = Jarmo.Palkka.ToString();
+            nimi4TextBlock.Text = Mieskolainen.Nimi;
+            palkka4TextBlock.Text = Mieskolainen.Palkka.ToString();
+
+        }
+
+        private void addPalkka1Button_Click(object sender, RoutedEventArgs e)
+        {
+            double muuttuja = double.Parse(palkka1TextBlock.Text);
+            palkka1TextBlock.Text = Ari.PalkanLasku(muuttuja).ToString();
+        }
+
+        private void addPalkka2Button_Click(object sender, RoutedEventArgs e)
+        {
+            double muuttuja = double.Parse(palkka2TextBlock.Text);
+            palkka2TextBlock.Text = Narsu.PalkanLasku(muuttuja).ToString();
+        }
+
+        private void addPalkka3Button_Click(object sender, RoutedEventArgs e)
+        {
+            double muuttuja = double.Parse(palkka3TextBlock.Text);
+            palkka3TextBlock.Text = Jarmo.PalkanLasku(muuttuja).ToString();
+        }
+
+        private void addPalkka4Button_Click(object sender, RoutedEventArgs e)
+        {
+            double muuttuja = double.Parse(palkka4TextBlock.Text);
+            palkka4TextBlock.Text = Mieskolainen.PalkanLasku(muuttuja).ToString();
+        }
+    }
+}
---
+++ harkkatyo/harkkatyo/Opettaja.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harkkatyo
+{
+    class Opettaja
+    {
+        private double palkka = 2000;
+
+        public string Nimi;
+
+        public double Palkka
+        {
+            get { return palkka; }
+            set { palkka = value;  }
+        }
+
+
+        public Opettaja()
+        {
+
+        }
+
+        public Opettaja(string nimi, double palkka)
+        {
+            Nimi = nimi;
+            Palkka = palkka;
+        }
+
+        public double PalkanLasku(double numero)
+        {
+            return numero+10;
+        }
+
+
+    }
+}
